ad80332219b5c4af8f2e4d6405d81967e0892815
FrontSites <teeteroki@gmail.com>
2025-07-28 12:56:40 +0300

    🔄 Auto-commit v153 - 🐘 PHP (28.07.2025, 12:56:40)

--- template-parts/buttons/sponsor-button.php
+++ template-parts/buttons/sponsor-button.php
@@ -1,4 +1,10 @@
 <a href="#" class="main-button _border">
   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hand.svg" alt="description">
-  <?php the_field("sponsor_button_text", "option"); ?>
+  <?php
+  if (function_exists('the_field')) {
+    the_field("sponsor_button_text", "option");
+  } else {
+    echo 'Sponsor';
+  }
+  ?>
 </a>
--- template-parts/buttons/ticket-button.php
+++ template-parts/buttons/ticket-button.php
@@ -1,4 +1,10 @@
 <a href="#" class="main-button ticket">
   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ticket.svg" alt="description">
-  <?php the_field("ticket_button_text", "option"); ?>
+  <?php 
+  if (function_exists('the_field')) {
+    the_field("ticket_button_text", "option");
+  } else {
+    echo 'Buy Ticket';
+  }
+  ?>
 </a>
