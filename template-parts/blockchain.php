2b81a48b25973a3d50700b100e9a433f401e5e91
FrontSites <teeteroki@gmail.com>
2025-07-28 18:40:50 +0300

    🚀 Build & Commit v335 - 📝 SCSS, 📜 JS, 🐘 PHP (28.07.2025, 18:40:50)

--- template-parts/blockchain.php
+++ template-parts/blockchain.php
@@ -2,7 +2,14 @@
   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blockchain.svg" alt="">
   <div class="container">
     <div class="block-chain-block">
-
+      <?php the_field("blockchain-title"); ?>
+      <div class="block-chain-block__list">
+        <?php the_field("blockchain-list"); ?>
+      </div>
+      <div class="block-chain-block__text">
+        <?php the_field("blockchain-text"); ?>
+      </div>
+      <?php get_template_part("template-parts/ticket-button"); ?>
     </div>
   </div>
 </section>
