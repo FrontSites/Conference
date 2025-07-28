4d6247058ca1a0e35648346d9298e599d5c25d88
FrontSites <teeteroki@gmail.com>
2025-07-28 10:26:38 +0300

    🔄 Auto-commit v29 - 🐘 PHP (28.07.2025, 10:26:38)

--- footer.php
+++ footer.php
@@ -1,20 +1,19 @@
 </main>
 
 <footer class="footer">
-      <div class="container">
-      <?php wp_nav_menu([
-    'theme_location' => 'footer-site-menu',
-    
-    'container'       => false,
-    'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-    'menu_class'     => 'footer-menu', // navbar-2nd_level
+  <div class="container">
+    <?php wp_nav_menu([
+      'theme_location' => 'footer-site-menu',
+
+      'container'       => false,
+      'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+      'menu_class'     => 'footer-menu', // navbar-2nd_level
     ]);
-?> 
-      </div>
-    </footer>
+    ?>
+  </div>
+</footer>
 </div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
-
-
