049dbb8ac3cc02d85491fc20e2086a0fc9f0bc67
FrontSites <teeteroki@gmail.com>
2025-07-28 12:42:29 +0300

    🔄 Auto-commit v141 - 📜 JS (28.07.2025, 12:42:29)

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => { 
 })
+
+
+function initMenu() {
+  function initMobileMenu() {
+    function setupMobileMenuHandlers() {
+      // Remove previous handlers
+      $(".btn_nav").off("click.mobileMenu");
+      $(".header-wrapper li:not(.menu-item-has-children) a").off("click.mobileMenu");
+  
+      if ($(window).width() <= 1160) {
+        $(".btn_nav").on("click.mobileMenu", function () {
+          $(".burger").toggleClass("active");
+          if ($(".header-wrapper").is(":hidden")) {
+            $("body").addClass("hidden");
+            setTimeout(function () {
+              $(".header-wrapper").slideDown(200);
+              $(".header-wrapper").css({ display: "flex" });
+            }, 100);
+          } else {
+            $(".header-wrapper").slideUp(200);
+            setTimeout(function () {}, 300);
+            $("body").removeClass("hidden");
+          }
+        });
+  
+        $(".header-wrapper li:not(.menu-item-has-children) a").on("click.mobileMenu", function () {
+          $(".header-wrapper").slideUp(200);
+          $(".burger").removeClass("active");
+          $("body").removeClass("hidden");
+        });
+      } else {
+        // Reset states if switching to desktop
+        $(".burger").removeClass("active");
+        $(".header-wrapper").removeAttr("style");
+        $("body").removeClass("hidden");
+      }
+    }
+  
+    // Init on load
+    setupMobileMenuHandlers();
+  
+    // Re-init on resize
+    $(window).on("resize.mobileMenu", function () {
+      setupMobileMenuHandlers();
+    });
+  }
+} 
