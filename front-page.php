869c5426fcd6f26a4092b4bd4f03d4051a5c3c6f
FrontSites <teeteroki@gmail.com>
2025-07-28 09:59:30 +0300

    🔄 Auto-commit v18 - 🐘 PHP (28.07.2025, 09:59:30)

--- front-page.php
+++ front-page.php
@@ -9,6 +9,7 @@ Template Name: Home
   <?php get_template_part('template-parts/hero'); ?>
 <?php get_template_part('template-parts/buttons/sponsor-button'); ?>
 <?php get_template_part('template-parts/buttons/speaker-button'); ?>
+<?php get_template_part('template-parts/buttons/button-mediapartner'); ?>
 
 
 
---
+++ template-parts/buttons/button-mediapartner.php
@@ -0,0 +1,3 @@
+<a href="#" class="main-button _border">
+  <?php the_field("button_mediapartner_text", "option"); ?>
+</a>
