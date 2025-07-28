7f1ff22f89677661614de896be4952c4c73b8b47
FrontSites <teeteroki@gmail.com>
2025-07-28 13:53:45 +0300

    🔄 Auto-commit v204 - 📝 SCSS, 🐘 PHP (28.07.2025, 13:53:44)

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,7 +1,7 @@
  <section class="hero">
    <div class="container">
      <div class="hero-items">
-       <div class="hero-item.left">
+       <div class="hero-item left">
          <div class="hero-item-content">
            <?php the_field("hero-title"); ?>
          </div>
