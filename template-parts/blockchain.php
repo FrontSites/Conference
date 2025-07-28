0f3de5cb04aac7d7f6a441f69d7620231be66d9f
FrontSites <teeteroki@gmail.com>
2025-07-28 18:39:37 +0300

    🚀 Build & Commit v334 - 🐘 PHP (28.07.2025, 18:39:37)

--- template-parts/blockchain.php
+++ template-parts/blockchain.php
@@ -1,5 +1,8 @@
 <section class="blockchain">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blockchain.svg" alt="">
   <div class="container">
+    <div class="block-chain-block">
 
+    </div>
   </div>
 </section>
