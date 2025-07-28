84da0b9196743f1871db36a4eec6d499122f32ce
FrontSites <teeteroki@gmail.com>
2025-07-28 12:57:11 +0300

    🔄 Auto-commit v154 - 🐘 PHP (28.07.2025, 12:57:11)

--- template-parts/buttons/speaker-button.php
+++ template-parts/buttons/speaker-button.php
@@ -1,4 +1,10 @@
 <a href="#" class="main-button _border">
   <img src="<?php echo get_template_directory_uri(); ?>/assets/images/micro.svg" alt="description">
-  <?php the_field("speaker_button_text", "option"); ?>
+  <?php 
+  if (function_exists('the_field')) {
+    the_field("speaker_button_text", "option");
+  } else {
+    echo 'Speaker';
+  }
+  ?>
 </a>
