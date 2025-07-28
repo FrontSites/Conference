60feabe8e421fb470a54f201be9757de7b649a0d
FrontSites <teeteroki@gmail.com>
2025-07-28 13:07:44 +0300

    🔄 Auto-commit v163 - 📝 SCSS (28.07.2025, 13:07:44)

--- footer.php
+++ footer.php
@@ -2,24 +2,18 @@
 
 <footer class="footer">
   <div class="container">
-    <?php 
-    if (has_nav_menu('footer-site-menu')) {
-      wp_nav_menu([
-        'theme_location' => 'footer-site-menu',
-        'container'       => false,
-        'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-        'menu_class'     => 'footer-menu',
-      ]);
-    }
+    <?php wp_nav_menu([
+      'theme_location' => 'footer-site-menu',
+
+      'container'       => false,
+      'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+      'menu_class'     => 'footer-menu', // navbar-2nd_level
+    ]);
     ?>
   </div>
 </footer>
 </div>
-<?php 
-error_log('Footer.php: before wp_footer()');
-wp_footer(); 
-error_log('Footer.php: after wp_footer()');
-?>
+<?php wp_footer(); ?>
 </body>
 
 </html>
