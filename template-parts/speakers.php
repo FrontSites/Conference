1c30ac221d40cf31bd5735c0cc372edd3fe134fb
FrontSites <teeteroki@gmail.com>
2025-07-30 12:23:11 +0300

    🚀 Build & Commit v387 - 📝 SCSS, 📜 JS, 🐘 PHP (30.07.2025, 12:23:11)

--- template-parts/speakers.php
+++ template-parts/speakers.php
@@ -1,5 +1,18 @@
 <section class="speakers">
   <div class="container">
-    <h2>Speakers</h2>
+    <h2><?php the_field("speakers-title"); ?></h2>
+    <div class="speakers-items">
+      <?php $speakers = get_field("speakers-items");
+      foreach ($speakers as $speaker) {
+        $image = $speaker["image"];
+        $name = $speaker["name"];
+        $position = $speaker["position"];
+
+      ?>
+        <div class="speakers-item">
+          <img src="<?php echo $speaker["image"]; ?>" alt="<?php echo $speaker["name"]; ?>">
+        </div>
+      <?php } ?>
+    </div>
   </div>
 </section>
