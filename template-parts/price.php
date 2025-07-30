b9986fd742066930a5012e1222dfc39c06e85f0e
FrontSites <teeteroki@gmail.com>
2025-07-30 19:03:12 +0300

    🚀 Build & Commit v634 - 🐘 PHP (30.07.2025, 19:03:11)

--- template-parts/price.php
+++ template-parts/price.php
@@ -4,42 +4,40 @@
       <?php the_field('price-title'); ?>
     </h2>
     <div class="price-items">
-      <?php $prices = get_field('price-items'); ?>
-      <?php foreach ($prices as $price) : ?>
-        <?php $title = $price['price-item-title']; ?>
-        <?php $description = $price['price-description']; ?>
-        <?php $price_old_price = $price['price-old-price']; ?>
-        <?php $price_new_price = $price['price-new-price']; ?>
-        <?php $price_small_label = $price['price-small-label']; ?>
-        <?php $price_discount = $price['price-discount']; ?>
-
-
-
-        <div class="price-item__wrapper">
-          <div class="price-item">
-            <h3><?php echo $price['title']; ?></h3>
-            <p><?php echo $description['description']; ?></p>
-            <div class="price-block">
-              <div class="price-block__item left">
-                <div class="price-block__old-price">
-                  <?php echo $price_old_price['price-old-price']; ?>
+      <?php
+      if (have_rows('price-items')) {
+        while (have_rows('price-items')) {
+          the_row();
+      ?>
+          <div class="price-item__wrapper">
+            <div class="price-item">
+              <h3><?php the_sub_field('price-item-title'); ?></h3>
+              <p><?php the_sub_field('price-description'); ?></p>
+              <div class="price-block">
+                <div class="price-block__item left">
+                  <div class="price-block__old-price">
+                    <s><?php the_sub_field('price-old-price'); ?></s>
+                  </div>
+                  <div class="price-block__new-price">
+                    <?php the_sub_field('price-new-price'); ?>
+                  </div>
                 </div>
-                <div class="price-block__new-price">
-                  <?php echo $price_new_price['price-new-price']; ?>
+                <div class="price-block__item right">
+                  <div class="price-block__small-label">
+                    <?php the_sub_field('price-small-label'); ?>
+                  </div>
+                  <div class="price-block__discount">
+                    <?php the_sub_field('price-discount'); ?>
+                  </div>
                 </div>
+                <?php get_template_part('template-parts/buttons/ticket-button'); ?>
               </div>
-              <div class="price-block__item right">
-
-              </div>
-              <?php get_template_part('template-parts/buttons/ticket-button'); ?>
-
             </div>
-
-
           </div>
-
-        </div>
-      <?php endforeach; ?>
+      <?php
+        }
+      }
+      ?>
     </div>
   </div>
 </section>
