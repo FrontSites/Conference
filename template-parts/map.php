9afd5d07f80986213d2abfb5985dcc21cc738fba
FrontSites <teeteroki@gmail.com>
2025-07-30 20:38:31 +0300

    🚀 Build & Commit v694 - 🐘 PHP (30.07.2025, 20:38:31)

--- template-parts/map.php
+++
@@ -1,9 +0,0 @@
-<section class="map-section">
-  <div class="container">
-    <h2><?php the_field('map-title'); ?></h2>
-    <p><?php the_field('map-subtitle'); ?></p>
-    <div class="map-wrapper">
-      <div id="map" class="google-map"></div>
-    </div>
-  </div>
-</section>
