85b84f92ac90fdacebe4b72784b19a073d124542
FrontSites <teeteroki@gmail.com>
2025-07-30 19:01:52 +0300

    🚀 Build & Commit v633 - 🐘 PHP (30.07.2025, 19:01:52)

--- template-parts/price.php
+++ template-parts/price.php
@@ -22,19 +22,24 @@
             <div class="price-block">
               <div class="price-block__item left">
                 <div class="price-block__old-price">
-                  <?php echo $price['price-old-price']; ?>
+                  <?php echo $price_old_price['price-old-price']; ?>
                 </div>
                 <div class="price-block__new-price">
-                  <?php echo $price['price-new-price']; ?>
+                  <?php echo $price_new_price['price-new-price']; ?>
                 </div>
               </div>
               <div class="price-block__item right">
-                /div>
-                <?php get_template_part('template-parts/buttons/ticket-button'); ?>
 
               </div>
+              <?php get_template_part('template-parts/buttons/ticket-button'); ?>
 
-            <?php endforeach; ?>
             </div>
+
+
           </div>
+
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
 </section>
