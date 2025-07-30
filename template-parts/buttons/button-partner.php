114125eabb47fa0c560797398c505cbe69f1aeb6
FrontSites <teeteroki@gmail.com>
2025-07-30 18:00:43 +0300

    🚀 Build & Commit v589 - 🐘 PHP (30.07.2025, 18:00:43)

---
+++ template-parts/buttons/button-partner.php
@@ -0,0 +1,10 @@
+<a href="#" class="main-button _border">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hand.svg" alt="description">
+  <?php
+  if (function_exists('the_field')) {
+    the_field("button_partner_text", "option");
+  } else {
+    echo 'Become a Partner';
+  }
+  ?>
+</a>
