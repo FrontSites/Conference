440bc638fc30879dbf66c33c4cb8ef67100672e8
FrontSites <teeteroki@gmail.com>
2025-07-30 13:45:58 +0300

    🚀 Build & Commit v442 - 🐘 PHP (30.07.2025, 13:45:58)

--- template-parts/schedule.php
+++ template-parts/schedule.php
@@ -1,7 +1,19 @@
 <section class="schedule">
   <div class="container">
     <div class="schedule-items">
+      <?php $schedule = get_field("schedule"); 
 
+      foreach ($schedule as $item) {
+        $title = $item["schedule-text"];
+        $time = $item["time"];
+        ?>
+        <div class="schedule-item">
+          <div class="schedule-item-title">
+            <?php echo $item["title"]; ?>
+          </div>
+        </div>
+        <?php
+      }
     </div>
   </div>
 </section>
