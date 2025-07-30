e32e1ce0859f872b12474f61f5f4db31631b9fc9
FrontSites <teeteroki@gmail.com>
2025-07-30 17:52:03 +0300

    🚀 Build & Commit v587 - 🐘 PHP (30.07.2025, 17:52:03)

--- template-parts/partners.php
+++ template-parts/partners.php
@@ -1,8 +1,14 @@
 <section class="partners">
   <div class="container">
-    <h2 class="partners-title">
-      <div class="partners-items">
-        <?php $partners = get_field('partners-items'); ?>
-      </div>
+    <h2><?php the_field('partners-title'); ?></h2>
+    <p><?php the_field('partners-subtitle'); ?></p>
+    <div class="partners-items">
+      <?php $partners = get_field('partners-items'); ?>
+      <?php foreach ($partners as $partner) : ?>
+        <div class="partners-item">
+          <img src="<?php echo $partner['image']['url']; ?>" alt="<?php echo $partner['partner-image']['alt']; ?>">
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
 </section>
