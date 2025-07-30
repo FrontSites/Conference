ddb92329bb2d0fedbc2edc1d262145f0d28256ef
FrontSites <teeteroki@gmail.com>
2025-07-30 18:48:18 +0300

    🚀 Build & Commit v630 - 🐘 PHP (30.07.2025, 18:48:18)

--- template-parts/price.php
+++ template-parts/price.php
@@ -10,16 +10,31 @@
         <?php $description = $price['price-description']; ?>
         <?php $price = $price['price-old-price']; ?>
         <?php $price = $price['price-new-price']; ?>
+        <?php $price = $price['price-small-label']; ?>
+        <?php $price = $price['price-small-label']; ?>
+
+
 
         <div class="price-item__wrapper">
           <div class="price-item">
             <h3><?php echo $price['title']; ?></h3>
             <p><?php echo $price['description']; ?></p>
-            <?php get_template_part('template-parts/buttons/ticket-button'); ?>
-          </div>
-        </div>
+            <div class="price-block">
+              <div class="price-block__item left">
+                <div class="price-block__old-price">
+                  <?php echo $price['price-old-price']; ?>
+                </div>
+                <div class="price-block__new-price">
+                  <?php echo $price['price-new-price']; ?>
+                </div>
+              </div>
+              <div class="price-block__item right">
+                /div>
+                <?php get_template_part('template-parts/buttons/ticket-button'); ?>
 
-      <?php endforeach; ?>
-    </div>
-  </div>
+              </div>
+
+            <?php endforeach; ?>
+            </div>
+          </div>
 </section>
