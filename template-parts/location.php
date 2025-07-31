8e18a3228b5c977c2f3a4fd505e8fe6a501c1ac3
FrontSites <teeteroki@gmail.com>
2025-07-31 10:34:50 +0300

    🚀 Build & Commit v717 - 🐘 PHP (31.07.2025, 10:34:50)

--- template-parts/location.php
+++ template-parts/location.php
@@ -3,7 +3,7 @@
     <h2><?php the_field('location-title'); ?></h2>
     <div class="location-wrapper">
       <div id="map" class="google-map"></div>
-      >
+
     </div>
     <div class="location-block">
       <a href="/#rent" class="location-item">
