3f70a6aaa808d4f6e95838cf0b98ac3ae98776c3
FrontSites <teeteroki@gmail.com>
2025-07-31 12:47:47 +0300

    🚀 Build & Commit v817 - 📝 SCSS (31.07.2025, 12:47:47)

--- footer.php
+++ footer.php
@@ -2,38 +2,38 @@
 </main>
 
 <footer class="footer">
-  <div class="container">
-    <div class="footer-items">
-      <div class="logo">
-        <a href="<?php echo home_url(); ?>">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.svg" alt="logo">
-        </a>
+  <div class="footer-wrapper">
+    <div class="container">
+      <div class="footer-items">
+        <div class="logo">
+          <a href="<?php echo home_url(); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.svg" alt="logo">
+          </a>
+        </div>
+
+
+        <div class="footer-menu">
+          <?php wp_nav_menu([
+            'theme_location' => 'footer-site-menu',
+
+            'container'       => false,
+            'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'menu_class'     => 'main-menu', // navbar-2nd_level
+          ]);
+          ?>
+        </div>
+
+        <div class="footer-links">
+          <a href="mailto:<?php echo get_theme_mod('contact_email', ''); ?></a>">
+            <?php echo get_theme_mod('contact_email', ''); ?>
+          </a>
+          <a href="tel:<?php echo get_theme_mod('contact_phone', ''); ?>">
+            <?php echo get_theme_mod('contact_phone', ''); ?>
+          </a>
+        </div>
+        <?php get_template_part('template-parts/buttons/ticket-button'); ?>
       </div>
-
-
-      <div class="footer-menu">
-        <?php wp_nav_menu([
-          'theme_location' => 'footer-site-menu',
-
-          'container'       => false,
-          'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-          'menu_class'     => 'main-menu', // navbar-2nd_level
-        ]);
-        ?>
-      </div>
-
-      <div class="footer-links">
-        <a href="mailto:<?php echo get_theme_mod('contact_email', ''); ?></a>">
-          <?php echo get_theme_mod('contact_email', ''); ?>
-        </a>
-        <a href="tel:<?php echo get_theme_mod('contact_phone', ''); ?>">
-          <?php echo get_theme_mod('contact_phone', ''); ?>
-        </a>
-      </div>
-      <?php get_template_part('template-parts/buttons/ticket-button'); ?>
     </div>
-  </div>
-
   </div>
 </footer>
 </div>
