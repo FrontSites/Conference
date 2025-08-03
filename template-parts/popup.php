ecead4f4b895bcd7f39854a28292408c58fd4e67
FrontSites <teeteroki@gmail.com>
2025-08-03 18:09:27 +0300

    🚀 Auto Commit v920 - 🐘 PHP (03.08.2025, 18:09:27)

--- template-parts/popup.php
+++ template-parts/popup.php
@@ -1,10 +1,9 @@
 <section class="popup-wrapper">
   <div class="popup">
+    .
     <div class="popup-wrapper__top">
-
       <h2><?php the_field("popup-title"); ?></h2>
-
     </div>
-
+    <?php echo do_shortcode('[contact-form-7 id="542cab5" title="Попап"]'); ?>
   </div>
 </section>
