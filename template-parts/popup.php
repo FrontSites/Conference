a56dab23eafe73ca8f81a1f10acc02f436624159
FrontSites <teeteroki@gmail.com>
2025-08-03 18:09:38 +0300

    🚀 Auto Commit v929 - 🐘 PHP (03.08.2025, 18:09:38)

--- template-parts/popup.php
+++ template-parts/popup.php
@@ -1,6 +1,9 @@
 <section class="popup-wrapper">
   <div class="popup">
-    .
+    <div class="close-button"><svg width="39" height="38" viewBox="0 0 39 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M38.5114 0.741868L1.74185 37.5114M37.7972 37.5114L1.02767 0.741866" stroke="white" />
+      </svg>
+    </div>
     <div class="popup-wrapper__top">
       <h2><?php the_field("popup-title"); ?></h2>
     </div>
