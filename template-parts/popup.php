8e1ae35b5cbe6506f0b06ef4e6cccaf1dc73dc2e
FrontSites <teeteroki@gmail.com>
2025-08-03 17:59:12 +0300

    🚀 Auto Commit v912 - 🐘 PHP (03.08.2025, 17:59:12)

--- template-parts/popup.php
+++ template-parts/popup.php
@@ -1,6 +1,8 @@
 <section class="popup-wrapper">
   <div class="popup">
-    <h3>Залиши свою інформацію, щоб ми могли зв’язатися з тобою та обговорити деталі партнерства</h3>
+    <div class="popup-wrapper__top">
+
+    </div>
 
   </div>
 </section>
