e54b7abfc93f8b3206b8902a2afe1c0982c33b9d
FrontSites <teeteroki@gmail.com>
2025-08-03 18:08:03 +0300

    🚀 Auto Commit v927 - 🐘 PHP (03.08.2025, 18:08:03)

--- template-parts/popup.php
+++ template-parts/popup.php
@@ -8,28 +8,3 @@
 
   </div>
 </section>
-
-<div class="form-items">
-  <div class="form-item">
-    [text* Name class:name placeholder "Name"]
-  </div>
-
-  <div class="form-item">
-    [tel* phone class:phone placeholder "+380__-___-__-__"]
-  </div>
-
-  <div class="form-item">
-    [email* email class:email placeholder "E-mail"]
-  </div>
-
-  <div class="form-item">
-    [select* choose class:select "Стати спікером" "Стати спонсором" "Стати медіапартнером" "Стати технічним партнером"]
-  </div>
-
-  <div class="form-item">
-    [text* company-name class:company placeholder "НГазва компанії"]
-
-  </div>
-
-  [submit class:main-button "Відправити"]
-</div>
