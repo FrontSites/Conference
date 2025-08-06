b00dcf1886473a1ad42a9276f280579af5d59840
FrontSites <teeteroki@gmail.com>
2025-08-06 12:21:36 +0300

    🚀 Auto Commit v1136 - 📜 JS (06.08.2025, 12:21:36)

--- src/js/main.js
+++ src/js/main.js
@@ -48,7 +48,7 @@ function initGoogleMaps() {
   const script = document.createElement("script");
   const language = window.mapConfig.language || 'uk';
   const timestamp = Date.now(); // Добавляем уникальный параметр
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${window.mapConfig.apiKey}&language=${language}&loading=async&callback=initLocationMap&v=weekly`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${window.mapConfig.apiKey}&language=${language}&callback=initLocationMap&v=weekly`;
   script.async = true;
   script.defer = true;
   
