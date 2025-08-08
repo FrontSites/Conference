d5ad060e303b0575de143fc8df375144551b3c3c
FrontSites <teeteroki@gmail.com>
2025-08-08 14:34:19 +0300

    🚀 Auto Commit v1385 - 🐘 PHP (08.08.2025, 14:34:19)

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,6 +1,7 @@
  <section class="hero">
    <div class="hero-background">
-     <img src="<?php the_field("hero-background"); ?>" alt="">
+     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/lines-.svg" alt="description">
+
    </div>
    <div class="container">
      <div class="hero-items">
