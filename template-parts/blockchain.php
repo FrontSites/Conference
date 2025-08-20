d68a94d1ce655d235ef8dcbb5176f6b78fc52784
FrontSites <teeteroki@gmail.com>
2025-08-20 14:54:13 +0300

    🚀 Auto Commit v1458 - 📝 SCSS, 🐘 PHP (20.08.2025, 14:54:13)

--- template-parts/blockchain.php
+++ template-parts/blockchain.php
@@ -1,5 +1,5 @@
 <section class="blockchain">
-  <div class="blockchein-voices"><?php the_field("blockchain-voices"); ?></div>
+  <div class="blockchain-voices"><?php the_field("blockchain-voices"); ?></div>
   <div class="blockchain-img-block man">
     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sched-image.png" alt="description">
     <div class="blockchain-img-text">
