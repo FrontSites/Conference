1e738958a9deded9c820eecfa913a5133064ffc8
FrontSites <teeteroki@gmail.com>
2025-09-11 10:26:43 +0300

    🚀 Auto Commit v1657 - 🐘 PHP (11.09.2025, 10:26:43)

--- front-page.php
+++ front-page.php
@@ -6,6 +6,7 @@ Template Name: Home
 <?php get_header(); ?>
 
 <main>
+  <?php get_template_part('template-parts/marque'); ?>
   <?php get_template_part('template-parts/hero'); ?>
   <div class="main-content">
     <?php get_template_part('template-parts/blockchain'); ?>
