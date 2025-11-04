cf1b1c90d2ada9aab4e47e7041a27b4e0254f77d
FrontSites <teeteroki@gmail.com>
2025-11-04 14:13:39 +0200

    🚀 Auto Commit v1886 - 🐘 PHP (04.11.2025, 14:13:39)

--- template-parts/app.php
+++ template-parts/app.php
@@ -4,15 +4,6 @@ if (is_admin()) : ?>
 
   <section class="app">
     <div class="container">
-      <div class="app-block">
-        <h2><?php the_field("app-title"); ?></h2>
-        <div class="app-block__description">
-          <?php the_field("app-description"); ?>
-        </div>
-        <div class="app-block__content">
-          <?php the_field("app-content"); ?>
-        </div>
-      </div>
     </div>
   </section>
 
