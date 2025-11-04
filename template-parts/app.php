d8ee1e6a9944cef832f7241a4c61b00ca2df39d8
FrontSites <teeteroki@gmail.com>
2025-11-04 14:15:02 +0200

    🚀 Auto Commit v1887 - 🐘 PHP (04.11.2025, 14:15:02)

--- template-parts/app.php
+++ template-parts/app.php
@@ -4,6 +4,14 @@ if (is_admin()) : ?>
 
   <section class="app">
     <div class="container">
+      <div class="app-items">
+        <div class="app-item left">
+          <div class="app-item__img">
+            <img src="<? the_field() ?>" alt="">
+          </div>
+        </div>
+        <div class="app-item right"></div>
+      </div>
     </div>
   </section>
 
